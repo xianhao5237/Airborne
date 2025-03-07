63b76ebddd477798ac5821cbd7ec9fa8654c650a
XianhaoZhou <xianhaozhou@gmail.com>
2025-03-07 15:26:43 -0500

    switch to cloud sql postgres db

--- app/database.py
+++ app/database.py
@@ -8,12 +8,10 @@ def get_connection():
     user = os.environ.get("USER")
     password = os.environ.get("PASS")
     port = os.environ.get("PORT")
-    
-    return psycopg2.connect(
-        database=db, 
-        host=host, 
-        user=user, 
-        password=password, 
-        port=port,
-        cursor_factory=RealDictCursor
-    )
+    url = os.environ.get("DATABASE_URL")
+
+    # #gcp cloud sql connection
+    # return psycopg2.connect(url, cursor_factory=RealDictCursor)
+
+    unix_socket = '/cloudsql/{}'.format(host)
+    return psycopg2.connect(database=db, user =user, password = password, host = unix_socket, cursor_factory=RealDictCursor)
